3b94df3d311de5d907ede5588dde7464385cedfc
Sohan Akolkar <sohanawscert2017@gmail.com>
2017-12-28 02:02:06 +0530

    final edit before production

--- lambda_function.py
+++ lambda_function.py
@@ -5,26 +5,44 @@ import mimetypes
 
 
 def lambda_handler(event, context):
-    
-    s3 = boto3.resource('s3')
-
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:197401872705:portfolioSNSTopic')
+    location = {
+            "bucketName": 'portfoliobuild.akolkarsohan.com',
+            "objectKey": 'portfoliobuild.zip'
 
-    portfolio_bucket = s3.Bucket('portfolio.akolkarsohan.com')
-    build_bucket = s3.Bucket('portfoliobuild.akolkarsohan.com')
-    
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    try:
+        job = event.get ("Codepipeline.job")
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm,
-                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3location"]
+
+        print "Building portfolio from" + str(location)    
+
+        s3 = boto3.resource('s3')
+
+        portfolio_bucket = s3.Bucket('portfolio.akolkarsohan.com')
+        build_bucket = s3.Bucket('portfoliobuild.akolkarsohan.com')
     
-    print "Job Done!"
-    topic.publish(Subject='Portfolio Deployment', Message = 'Portfolio Deployment is successful.')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm,
+                        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    
+        print "Job Done!"
+        topic.publish(Subject='Portfolio Deployment Succeeded', Message = 'Portfolio Deployment is successful.')
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId = job["id"])
+    except:
+        topic.publish(Subject='Portfolio Deployment Failed', Message = 'Portfolio Deployment is not successful.')
+        raise
     return 'Hello from Lambda'
 
